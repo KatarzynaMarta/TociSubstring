8f7d2d48eadb23e909dc6ce34b9773ade432e35e
Katarzyna Kuich <89656991+KatarzynaKuich@users.noreply.github.com>
2022-04-13 10:07:53 +0200

    Dodaj pliki projektów.

---
+++ Benchmark.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static System.Console;
+
+WriteLine("Hello, World!");
+
+namespace TociSubstring
+{
+    public class Benchmark
+    {
+        
+        //Substring("beatka",2,3) >ea
+        public virtual string Substring(string candidate, int position, int lenght)
+        {
+            string result = string.Empty;
+            for (var i = position; i < lenght; i++)
+            {
+                result += candidate[i++];
+            }
+
+            return result;
+        }
+    }
+} 
